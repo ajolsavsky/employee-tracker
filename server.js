07aca147010e409a433228a25a2ce0a69ba0cba1
Alexandra Olsavsky <alexandraolsavsky@gmail.com>
2022-06-27 07:45:14 -0500

    Updated server.js with protected credentials

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
 const express = require('express');
+const sequelize = require('./config/connection');
 const mysql = require('mysql2');
 
-const PORT = process.env.PORT || 3001;
+
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Connect to database
-//TODO: Replace with secure vs
-const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      // MySQL username,
-      user: 'root',
-      // TODO: Add MySQL password here
-      password: '',
-      database: 'movies_db'
-    },
-    console.log(`Connected to the movies_db database.`)
-);
-
-//TODO - post/get/delete requests
-
-
-app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`);
+sequelize.sync().then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
 
+
+// const express = require('express');
+// const mysql = require('mysql2');
+
+// const PORT = process.env.PORT || 3001;
+// const app = express();
+
+// // Express middleware
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+
+
+
+// app.listen(PORT, () => {
+// console.log(`Server running on port ${PORT}`);
+// });
   
